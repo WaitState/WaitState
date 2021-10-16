586a95dd47141a5cfcd774a35c6f510b4c941263
jake imanaka <jake.imanaka@gmail.com>
2021-10-16 01:34:16 -1000

    searchbox base

--- app/imports/ui/components/MenuBar.jsx
+++ app/imports/ui/components/MenuBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Meteor } from "meteor/meteor";
+import { styled, alpha } from "@mui/material/styles"
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -7,7 +8,46 @@ import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search"
+import InputBase from "@mui/material/InputBase";
 import AccountCircle from "@mui/icons-material/AccountCircle";
+import { ClassNames } from "@emotion/react";
+
+const SearchBox = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
 
 const MenuBar = (props) => {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -22,7 +62,7 @@ const MenuBar = (props) => {
   };
 
   const profileMenuId = "profileMenu";
-  renderProfileMenu = (
+  const renderProfileMenu = (
     <Menu
       id={profileMenuId}
       anchorEl={anchorEl}
@@ -56,7 +96,15 @@ const MenuBar = (props) => {
           >
             <MenuIcon />
           </IconButton>
-          <Box sx={{ flexGrow: 1 }} />
+          <SearchBox>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase 
+              placeholder="Search hospitals"
+              inputProps={{ 'aria-label': 'search'}}
+            />
+          </SearchBox>
           <IconButton
             size="large"
             aria-label="user account"
