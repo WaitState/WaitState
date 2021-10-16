32be545c2dd8cc58989a45b2429678aa6160957b
Tsz Ching Wong <tsz_wong@datahouse.com>
2021-10-15 23:16:02 -1000

    added register page

---
+++ app/imports/ui/pages/Register.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Meteor } from "meteor/meteor";
+const Register = (props) => {
+    return (
+        <div>
+            Register Page
+        </div>
+    );
+};
+
+export default Register;
