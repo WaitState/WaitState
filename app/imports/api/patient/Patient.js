af0c298b6ec97f0d7c713bd00beef175e736ff40
Justin Wong <justin_wong@datahouse.com>
2021-10-16 18:31:42 -1000

    Created Patient Collection

---
+++ app/imports/api/patient/Patient.js
@@ -0,0 +1,19 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+const Patients = new Mongo.Collection('Patients');
+
+const PatientSchema = new SimpleSchema({
+  patientID: String,
+  firstName: String,
+  lastName: String,
+  reason: String,
+  checkInStatus: Boolean,
+  checkInTime: Date,
+  email: String,
+}, { tracker: Tracker });
+
+Patients.attachSchema(PatientSchema);
+
+export { Patients, PatientSchema };
