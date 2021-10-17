8288a812cb3ee40a321b2f4de0dfcade0510d300
jake imanaka <jake.imanaka@gmail.com>
2021-10-17 04:07:18 -1000

    page loader

--- app/imports/ui/pages/Directory.jsx
+++ app/imports/ui/pages/Directory.jsx
@@ -1,19 +1,41 @@
 import React from "react";
 import { Meteor } from "meteor/meteor";
+import { withTracker } from 'meteor/react-meteor-data'
+import PropTypes from "prop-types";
 import { Hospitals } from "../../api/hospital/Hospital";
-import { Container, Box } from "@mui/material";
+import { Container, Box, CircularProgress, Backdrop } from "@mui/material";
 
 const Directory = (props) => {
+    const { ready } = props;
 
     return (
         <Box>
-            <Container>
-                <div>
-                    hello
-                </div>
-            </Container>
+            {!Boolean(ready) ? (
+                <Backdrop
+                    sx={{ color: '#fff', zIndex: (theme => theme.zIndex.drawer + 1 )}}
+                    open={true}
+                >
+                    <CircularProgress color="inherit" />
+                </Backdrop>
+            ) : (
+                <Container>
+                    
+                </Container>
+            )}
         </Box>
-    )
+    );
+};
+
+Directory.propTypes = {
+    ready: PropTypes.bool.isRequired,
 }
 
-export default Directory;
+const DirectoryContainer = withTracker(() => {
+    const subscription = Meteor.subscribe('Hospital');
+
+    return {
+        ready: subscription.ready(),
+    }
+})(Directory);
+
+export default DirectoryContainer;
