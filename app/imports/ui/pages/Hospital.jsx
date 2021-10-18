a28ff06795ef797e873ba70bea3c7dfbdee9015e
jake imanaka <jake.imanaka@gmail.com>
2021-10-18 00:31:24 -1000

    props, tracker, and imports

--- app/imports/ui/pages/Hospital.jsx
+++ app/imports/ui/pages/Hospital.jsx
@@ -3,6 +3,10 @@ import { useState, useEffect } from "react";
 import { Meteor } from "meteor/meteor";
 import { styled } from "@mui/system";
 import { Button, Input, Typography, Paper} from "@mui/material";
+import { useParams } from "react-router-dom";
+import { withTracker } from "meteor/react-meteor-data";
+import { Hospitals } from "../../api/hospital/Hospital";
+import PropTypes from "prop-types";
 
 const Container = styled(Paper)({
   display: "flex",
@@ -14,7 +18,10 @@ const Container = styled(Paper)({
   width: "250px",
 });
 
-export default Hospital = ({ history }) => {
+const HospitalPage = (props) => {
+
+  const { ready } = props;
+
   return (
     <Container>
        <Typography variant="h3"> Hospital
@@ -29,4 +36,18 @@ export default Hospital = ({ history }) => {
        </Typography>
     </Container>
   );
-};
+}
+
+HospitalPage.propTypes = {
+  ready: PropTypes.bool.isRequired,
+}
+
+const HospitalPageContainer = withTracker(() => {
+  const subscription = Meteor.subscribe('Hospital');
+
+  return {
+    ready: subscription.ready(),
+  };
+})(HospitalPage);
+
+export default HospitalPageContainer;
